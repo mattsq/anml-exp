d235a819984e7e9d78cd099f7410124cb37fab7a
Matthew Simmons <mattsq@users.noreply.github.com>
2025-07-20 19:02:46 +1000

    Add wine and digits datasets

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -79,3 +79,51 @@ def test_breast_cancer_shapes() -> None:
     assert y_test is not None
     assert y_test.ndim == 1
 
+
+def test_wine_deterministic() -> None:
+    X_train1, y_train1 = load_dataset("wine", split="train", seed=0)
+    X_train2, y_train2 = load_dataset("wine", split="train", seed=0)
+    X_test1, y_test1 = load_dataset("wine", split="test", seed=0)
+    X_test2, y_test2 = load_dataset("wine", split="test", seed=0)
+
+    assert y_train1 is None
+    assert y_train2 is None
+
+    npt.assert_array_equal(X_train1, X_train2)
+    npt.assert_array_equal(X_test1, X_test2)
+    npt.assert_array_equal(y_test1, y_test2)
+
+
+def test_wine_shapes() -> None:
+    X_train, y_train = load_dataset("wine", split="train")
+    X_test, y_test = load_dataset("wine", split="test")
+
+    assert y_train is None
+    assert X_train.shape[1] == X_test.shape[1] == 13
+    assert y_test is not None
+    assert y_test.ndim == 1
+
+
+def test_digits_deterministic() -> None:
+    X_train1, y_train1 = load_dataset("digits", split="train", seed=0)
+    X_train2, y_train2 = load_dataset("digits", split="train", seed=0)
+    X_test1, y_test1 = load_dataset("digits", split="test", seed=0)
+    X_test2, y_test2 = load_dataset("digits", split="test", seed=0)
+
+    assert y_train1 is None
+    assert y_train2 is None
+
+    npt.assert_array_equal(X_train1, X_train2)
+    npt.assert_array_equal(X_test1, X_test2)
+    npt.assert_array_equal(y_test1, y_test2)
+
+
+def test_digits_shapes() -> None:
+    X_train, y_train = load_dataset("digits", split="train")
+    X_test, y_test = load_dataset("digits", split="test")
+
+    assert y_train is None
+    assert X_train.shape[1] == X_test.shape[1] == 64
+    assert y_test is not None
+    assert y_test.ndim == 1
+
